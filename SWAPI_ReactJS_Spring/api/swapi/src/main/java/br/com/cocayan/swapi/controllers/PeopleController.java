c62a421a6828b851d541f41a14c501d8998bca3d
EliasMura <elias-murat@outlook.com>
2022-07-15 11:35:02 -0300

    criando people controller

---
+++ SWAPI_ReactJS_Spring/api/swapi/src/main/java/br/com/cocayan/swapi/controllers/PeopleController.java
@@ -0,0 +1,12 @@
+package br.com.cocayan.swapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+    
+    
+
+}
