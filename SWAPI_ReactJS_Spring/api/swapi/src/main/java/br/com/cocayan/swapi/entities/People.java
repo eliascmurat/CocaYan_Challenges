4d48b1ed94a549f43c49586397ce5b7fa018471e
EliasMura <elias-murat@outlook.com>
2022-07-17 18:19:00 -0300

    criando repository e add anotacoes no people

--- SWAPI_ReactJS_Spring/api/swapi/src/main/java/br/com/cocayan/swapi/entities/People.java
+++ SWAPI_ReactJS_Spring/api/swapi/src/main/java/br/com/cocayan/swapi/entities/People.java
@@ -2,22 +2,39 @@ package br.com.cocayan.swapi.entities;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Table(name = "people")
+@Entity
 public class People {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "peopleid")
     private Long peopleId;
-    
+
+    @Column(name = "name")
     private String name;
     
+    @Column(name = "height")
     private float height;
 
+    @Column(name = "mass")
     private float mass;
 
-    private LocalDateTime created = LocalDateTime.now();
+    @Column(name = "created")
+    private LocalDateTime created;
     
-    private LocalDateTime updated = LocalDateTime.now();
+    @Column(name = "updated")
+    private LocalDateTime updated;
 
     public People(String name, float height, float mass) {
         this.name = name;
---
+++ SWAPI_ReactJS_Spring/api/swapi/src/main/java/br/com/cocayan/swapi/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package br.com.cocayan.swapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cocayan.swapi.entities.People;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<People, Long> { }
