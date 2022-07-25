07b2122ecc9694800ed1c6cdff065069cf403672
EliasMura <elias-murat@outlook.com>
2022-07-25 10:02:14 -0300

    criando projeto clock - threads em java

---
+++ CLOCK_Threads_Java/clock/src/main/java/br/com/cocayan/Main.java
@@ -0,0 +1,13 @@
+package br.com.cocayan;
+
+/* 
+ * @author Elias Murat
+ * @sinze  2022-07-25
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, World!" );
+    }
+}
