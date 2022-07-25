d2139fa025290ba388e1969bc51a0908840b8066
EliasMura <elias-murat@outlook.com>
2022-07-25 11:54:37 -0300

    finalizando prj

--- CLOCK_Threads_Java/clock/src/main/java/br/com/cocayan/Clock.java
+++ CLOCK_Threads_Java/clock/src/main/java/br/com/cocayan/Clock.java
@@ -1,16 +1,39 @@
 package br.com.cocayan;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Clock implements Runnable {
 
-    private int i = 0;
+    // Lista de horarios
+    private List<LocalTime> listLocalTime = new ArrayList<>();
+    
+    // Horario atual
+    private LocalTime now = LocalTime.now();
 
+    // Construtor do Clock
+    public Clock(List<LocalTime> listLocalTime) {
+        this.listLocalTime = listLocalTime;
+    }
+
+    // Função para pegar a diferença entre os horarios
+    private Long getDifInMilis(LocalTime t1, LocalTime t2) {
+        return Duration.between(t1, t2).toMillis();
+    }
+
+    // Run
     @Override
     public void run() {
         while (true) {
             try {
-                System.out.println(i);
-                Thread.sleep(1000);
-                i++;
+                for (LocalTime localTime : this.listLocalTime) {
+                    Long dif = getDifInMilis(this.now, localTime);
+                    System.out.println("Time atual = " + localTime.getHour() + ":" + localTime.getMinute() + ":" + localTime.getSecond());
+                    System.out.println("A mimir...");
+                    Thread.sleep(dif);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- CLOCK_Threads_Java/clock/src/main/java/br/com/cocayan/Main.java
+++ CLOCK_Threads_Java/clock/src/main/java/br/com/cocayan/Main.java
@@ -1,14 +1,24 @@
 package br.com.cocayan;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /* 
  * @author Elias Murat
  * @sinze  2022-07-25
  */
 public class Main {
     public static void main(String[] args) {
-        Clock clock = new Clock();
-        Thread thread = new Thread(clock);
+        // Lista que vai ser passada como param no construtor da Class Clock
+        List<LocalTime> times = new ArrayList<>();
 
-        thread.start();
+        // Add tempo do "alarme"
+        times.add(LocalTime.parse("11:45:00"));
+        times.add(LocalTime.parse("11:46:00"));
+
+        // Cria Thread passando a classe Clock com implements Runnable como param e
+        // na classe Clock passando a lista de horarios
+        new Thread(new Clock(times)).start();
     }
 }
