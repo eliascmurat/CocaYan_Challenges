d2ce1236da40655c8eb1ecbedbcbb126124b11d2
EliasMura <elias-murat@outlook.com>
2022-07-27 18:11:27 -0300

    add anotacoes no entity de user

--- CLOCK_Threads_Spring/clock/src/main/java/br/com/cocayan/clock/entities/User.java
+++ CLOCK_Threads_Spring/clock/src/main/java/br/com/cocayan/clock/entities/User.java
@@ -1,5 +1,12 @@
 package br.com.cocayan.clock.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,12 +14,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "firstName")
     private String firstName;
 
+    @Column(name = "lastName")
     private String lastName;
 
 }
