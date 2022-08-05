24038a67cffd47a688ac4876c19f40d2c5f91f60
EliasMurat <elias-murat@outlook.com>
2022-08-05 15:04:47 -0300

    criando shared comp. e removendo classes n utilizadas

---
+++ SWAPI_Angular/src/app/shared/buttons-navigation/buttons-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-navigation',
+  templateUrl: './buttons-navigation.component.html',
+  styleUrls: ['./buttons-navigation.component.css']
+})
+export class ButtonsNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ SWAPI_Angular/src/app/shared/buttons-navigation/buttons-navigation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonsNavigationComponent } from './buttons-navigation.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonsNavigationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonsNavigationModule { }
